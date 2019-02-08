be46b5f1c87e5276800c8889074cedfcd226ea8c
katerina <katuha_maksimova@mail.ru>
2019-02-08 17:05:06 +0300

    create history in localStorage

--- src/js.js
+++ src/js.js
@@ -1,7 +1,3 @@
-document.getElementById
-
-
-
 document.getElementById('input-startdate-flights').min = new Date().toISOString().split('T')[0];
 
 document.getElementById('input-startdate-flights').onchange = function () {
@@ -28,22 +24,6 @@ document.getElementById('input-startdate-cars').onchange = function () {
     document.getElementById('input-enddate-cars').min = new Date(dt).toISOString().split('T')[0];
 }
 
-
-/* function requestsProcessing(func, ...value) {
-    var status = function (response) {
-        if (response.status !== 200) {
-            return Promise.reject(new Error(response.statusText))
-        }
-        return Promise.resolve(response)
-    }
-    var json = function (response) {
-        return response.json()
-    }
-    func(status, json, ...value).then(function (data) { addCountries(data); });
-} */
-
-/* requestsProcessing(requestCountries); */
-
 function request(url) {
     var status = function (response) {
         if (response.status !== 200) {
@@ -68,7 +48,7 @@ function requestCountries() {
     return request(url)
 }
 
-function requestCities(countryId, currentTab) {
+function requestCities(countryId) {
     const url = `http://api.geonames.org/searchJSON?country=${countryId}&username=kathrynowy`;
     return request(url)
 }
@@ -84,7 +64,7 @@ function onChangeCountry(event) {
     option.innerHTML = 'choose city';
     option.value = 'default';
     selectHotel.appendChild(option);
-    requestCities(event.target.value, currentTab)
+    requestCities(event.target.value)
         .then(function (data) {
             addCities(data.geonames, currentTab)
         })
@@ -111,7 +91,21 @@ function addCountries(data) {
         selectCars.appendChild(newOption);
     }
 }
-
-
+var arrHistory = [];
 document.getElementById('select-country-hotels').addEventListener('change', function () { onChangeCountry(event); });
 document.getElementById('select-country-cars').addEventListener('change', function () { onChangeCountry(event); });
+var keyForHistory = 'key';
+document.getElementById('hotelsSub').addEventListener('submit', function () {
+    var count = document.getElementById('hotelsSub').elements.length - 2;
+    var paste = document.getElementById('paste');
+    var objectWithData = {};
+    for (let i = 0; i < count; i++) {
+        var value = document.getElementById('hotelsSub').elements[i].value;
+        var id = document.getElementById('hotelsSub').elements[i].id;
+        if (value != 0 && value != undefined && value != null) {
+            objectWithData[id] = value;
+        }
+    }
+    arrHistory.push(JSON.stringify(objectWithData));
+    localStorage.setItem(keyForHistory, arrHistory);
+})
